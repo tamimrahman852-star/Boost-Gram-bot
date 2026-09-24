e6581e4c41fcf1123a283aa1c7ab8a005366f27a
tamim852 <realmoney852@gmail.com>
2026-09-24 20:47:14 +0600

    Update main.py

--- main.py
+++ main.py
@@ -12,12 +12,23 @@ from helpers import alert_admins, esc
 from web import fastapi_app
 from workers import retention_worker
 
-# Handlers import
+# Handlers import (Puraton ebong Notun shobai)
 from handlers.start import router as start_router
+from handlers.base_menu import router as base_menu_router
 from handlers.cabinet import router as cabinet_router
 from handlers.earnings import router as earnings_router
+from handlers.earn_channel import router as earn_channel_router
+from handlers.earn_group import router as earn_group_router
+from handlers.earn_view import router as earn_view_router
+from handlers.earn_bot import router as earn_bot_router
+from handlers.earn_reaction import router as earn_reaction_router
+from handlers.earn_boost import router as earn_boost_router
 from handlers.promote import router as promote_router
 from handlers.checks import router as checks_router
+from handlers.sub_check import router as sub_check_router
+from handlers.bots_partners import router as bots_partners_router
+from handlers.links import router as links_router
+from handlers.guide import router as guide_router
 from handlers.static import router as static_router
 from handlers.admin import router as admin_router
 
@@ -38,12 +49,23 @@ async def main():
     dp.message.outer_middleware(DBSessionMiddleware())
     dp.callback_query.outer_middleware(DBSessionMiddleware())
 
-    # Routers registration
+    # Routers registration (Order is important for proper handling)
     dp.include_router(start_router)
+    dp.include_router(base_menu_router)
     dp.include_router(cabinet_router)
     dp.include_router(earnings_router)
+    dp.include_router(earn_channel_router)
+    dp.include_router(earn_group_router)
+    dp.include_router(earn_view_router)
+    dp.include_router(earn_bot_router)
+    dp.include_router(earn_reaction_router)
+    dp.include_router(earn_boost_router)
     dp.include_router(promote_router)
     dp.include_router(checks_router)
+    dp.include_router(sub_check_router)
+    dp.include_router(bots_partners_router)
+    dp.include_router(links_router)
+    dp.include_router(guide_router)
     dp.include_router(static_router)
     dp.include_router(admin_router)
 
